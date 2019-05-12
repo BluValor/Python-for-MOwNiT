20b1a0f7476e43e1cc9b9f83ce84ddba83f12354
BluValor <jazajma@gmail.com>
2019-05-12 13:17:38 +0200

    lab 2 full

--- matlab_lab_2/ex1/gj_diagonal_pivot.m
+++
@@ -1,7 +0,0 @@
-function gj_diagonal_pivot(A)
-
-for m = 1:(size(A) - 1)
-    for i = (m + 1):size(A)
-        A(:, i) = A(:, i) + 10
-    end
-end
--- matlab_lab_2/ex1/run_ex_1.m
+++
@@ -1,7 +0,0 @@
-function run_ex_1()
-
-A = 100 * rand(10,10)
-
-gj_diagonal_pivot(A)
-
-disp(A)
